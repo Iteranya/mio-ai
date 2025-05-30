d4be4f800203286919e1cc7e49c29eb281b34ac8
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2025-05-30 12:09:31 +0700

    Added finance book
    I'm gonna stop for a bit and disassociate
    
    What am I doing with my life?
    
    Aaaaaaaaa-

--- models.py
+++ models.py
@@ -5,9 +5,16 @@ class DailyEntry:
     id:str
     raw_text:str
     csv:str
-    notes:str
+    report:str
     created:int
     updated:int
     error: str
 
-
+@dataclass
+class FinanceBook:
+    id:str
+    title:str
+    description:str
+    read_perm:list[str]
+    edit_perm:list[str]
+    gsheet_link: str
